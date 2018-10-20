bd1fe909da0f11a4aa1592b95825eb8a650c3ff4
lapsusone <6044855+Lapsusone@users.noreply.github.com>
2018-10-20 11:07:31 -0400

    Auto stash before revert of "Added infinite scrollable list for future dates"

--- ClientApp/components/fetch-data.vue
+++ ClientApp/components/fetch-data.vue
@@ -1,100 +1,107 @@
 ﻿<template>
     <div>
-        <h1>Weather forecast</h1>
-
-        <p>This component demonstrates fetching data from the server.</p>
-
-        <div v-if="!forecasts" class="text-center">
-            <p><em>Loading...</em></p>
-            <h1><icon icon="spinner" pulse/></h1>            
+               <div class="listWrap">
+            <VirtualList class="list"
+                :size="50"
+                :remain="6"
+                :tobottom="toBottom"
+            >
+                <div class="item" v-for="(day, index) of items" :index="index" :key="index">{{ day }}
+                </div>
+            </VirtualList>
+            <Loading class="list-loading" :loading="loading"></Loading>
         </div>
 
-        <template v-if="forecasts">
-            <table class="table">
-                <thead  class="bg-dark text-white">
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr :class="index % 2 == 0 ? 'bg-white' : 'bg-light'" v-for="(forecast, index) in forecasts" :key="index">
-                        <td>{{ forecast.dateFormatted }}</td>
-                        <td>{{ forecast.temperatureC }}</td>
-                        <td>{{ forecast.temperatureF }}</td>
-                        <td>{{ forecast.summary }}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <nav aria-label="...">
-                <ul class="pagination justify-content-center">
-                    <li :class="'page-item' + (currentPage == 1 ? ' disabled' : '')">
-                        <a class="page-link" href="#" tabindex="-1" @click="loadPage(currentPage - 1)">Previous</a>
-                    </li>
-                    <li :class="'page-item' + (n == currentPage ? ' active' : '')" v-for="(n, index) in totalPages" :key="index">
-                        <a class="page-link" href="#" @click="loadPage(n)">{{n}}</a>
-                    </li>
-                    <li :class="'page-item' + (currentPage < totalPages ? '' : ' disabled')">
-                        <a class="page-link" href="#" @click="loadPage(currentPage + 1)">Next</a>
-                    </li>
-                </ul>
-            </nav>
-        </template>
     </div>
 </template>
 
 <script>
-export default {
-  computed: {
-    totalPages: function () {
-      return Math.ceil(this.total / this.pageSize)
+    import Loading from './loading.vue'
+    import VirtualList from 'vue-virtual-scroll-list'
+import moment from 'moment';
+
+const getList = (length,times) => {
+    var arr = new Array(length);
+        for (var i = 0; i < length; i++) { 
+    arr[i] = moment().add(times*length+i, 'days').format('dddd D MMMM');
+}
+        return arr;
     }
-  },
+
+export default {
+  components: {  VirtualList, Loading },
 
   data () {
     return {
-      forecasts: null,
-      total: 0,
-      pageSize: 5,
-      currentPage: 1
+                        loading: false,
+                        times:0,
+      items: getList(20, 0)
     }
   },
 
-  methods: {
-    async loadPage (page) {
-      // ES2017 async/await syntax via babel-plugin-transform-async-to-generator
-      // TypeScript can also transpile async/await down to ES5
-      this.currentPage = page
-
-      try {
-        var from = (page - 1) * (this.pageSize)
-        var to = from + this.pageSize
-        let response = await this.$http.get(`/api/weather/forecasts?from=${from}&to=${to}`)
-        console.log(response.data.forecasts)
-        this.forecasts = response.data.forecasts
-        this.total = response.data.total
-      } catch (err) {
-        window.alert(err)
-        console.log(err)
-      }
-      // Old promise-based approach
-      // this.$http
-      //    .get('/api/SampleData/WeatherForecasts')
-      //    .then(response => {
-      //        console.log(response.data)
-      //        this.forecasts = response.data
-      //    })
-      //    .catch((error) => console.log(error))*/
-    }
-  },
-
-  async created () {
-    this.loadPage(1)
-  }
+        methods: {
+            toBottom () {
+                if (!this.loading) {
+                    this.loading = true
+                    // Mock for requesting delay.
+                    setTimeout(() => {
+                        this.times++
+                        this.loading = false
+                        this.items = this.items.concat(getList(20,this.times))
+                    }, 2017)
+                }
+            }
+        }
 }
 </script>
 
 <style>
+    .counter {
+        position: relative;
+        padding-bottom: 20px;
+    }
+    .count {
+        position: absolute;
+        right: 0;
+    }
+    .listWrap {
+        position: relative;
+    }
+    .list-loading {
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+    .list {
+        background: #fff;
+        border-radius: 3px;
+        border: 1px solid #ddd;
+        -webkit-overflow-scrolling: touch;
+        overflow-scrolling: touch;
+    }
+    .source {
+        text-align: center;
+        padding-top: 20px;
+    }
+    .source a {
+        color: #999;
+        text-decoration: none;
+        font-weight: 100;
+    }
+    @media (max-width: 640px) {
+        .times, .count {
+            display: block;
+        }
+        .count {
+            position: relative;
+        }
+    }
+
+        .item {
+        height: 50px;
+        line-height: 50px;
+        padding-left: 20px;
+        border-bottom: 1px solid #eee;
+    }
 </style>
