db85b10ad54f7c4e36cf8ba5ce4dc9a24e06cf45
Darren Webb <Darren.Webb@bd.com>
2018-10-30 08:26:01 -0400

    Added a create route button

--- ClientApp/components/home-page.vue
+++ ClientApp/components/home-page.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <h1>Welcome to Chariots of Trails!</h1>
-        
+        <form action="https://www.strava.com/routes/new/" target="_blank">
+            <input type="submit" value="Create a Route" class="button" />
+        </form>
     </div>
 </template>
 
