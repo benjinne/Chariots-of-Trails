1a351650cdfb0714b09a869b6688023617fd7761
Ben <benjinne@gmail.com>
2018-12-09 19:30:49 -0500

    fixed issue where any page would load before re-routing to login

--- ClientApp/router/index.js
+++ ClientApp/router/index.js
@@ -1,12 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import { routes } from './routes'
-
-Vue.use(VueRouter)
+import axios from 'axios'
 
 let router = new VueRouter({
   mode: 'history',
   routes
 })
+
+router.beforeEach(async (to, from, next) => {
+  console.log('checking if logged in')
+  if(to.path != '/login'){
+    if(await sessionExistsTest()){
+      next()
+    }else{
+      next('login')
+    }
+  }else{
+    next()
+  }
+})
+
+Vue.use(VueRouter)
+
+//test to see if logged in
+async function sessionExistsTest(){
+  let test = await axios.get(`/api/strava/sessionTest`)
+  return test.data
+}
 
 export default router
